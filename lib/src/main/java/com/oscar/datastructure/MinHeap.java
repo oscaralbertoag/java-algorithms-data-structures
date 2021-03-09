220470fa7e5ba4e48f7f5c0e970dc56e0a932a03
oscaralbertoag <oaahh4@mst.edu>
2021-03-08 20:55:45 -0700

    Add full implementation and tests of min heap tree data structure

---
+++ lib/src/main/java/com/oscar/datastructure/MinHeap.java
@@ -0,0 +1,176 @@
+package com.oscar.datastructure;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinHeap {
+
+    private Integer[] data;
+    private int size;
+
+    /**
+     * Builds a min heap tree. If a null initial capacity is provided, the tree will default to 20 and grow as needed
+     *
+     * @param initialCapacity initial capacity of the heap tree
+     */
+    public MinHeap(Integer initialCapacity) {
+        this.data = new Integer[initialCapacity == null ? 20 : initialCapacity];
+        this.size = 0;
+    }
+
+    /**
+     * Adds an element to the min heap tree
+     *
+     * @param element new date for the node to insert
+     */
+    public void add(Integer element) {
+        ensureCapacity();
+        data[size++] = element;
+        heapifyUp();
+    }
+
+    private void heapifyUp() {
+        Integer current = data[size - 1];
+        int currentIdx = size - 1;
+        while (hasParent(currentIdx)) {
+            if (getParent(currentIdx) > current) {
+                swap(currentIdx, getParentIndex(currentIdx));
+            }
+            current = getParent(currentIdx);
+            currentIdx = getParentIndex(currentIdx);
+        }
+    }
+
+    private void ensureCapacity() {
+        if (size == data.length) {
+            Integer[] increasedCapacity = new Integer[data.length + 20];
+            for (int i = 0; i < data.length; i++) {
+                increasedCapacity[i] = data[i];
+            }
+            data = increasedCapacity;
+        }
+    }
+
+    /**
+     * Returns the root of the tree (i.e. the minimum value in the tree)
+     *
+     * @return minimum value stored in the tree. Throws an exception if the tree is empty
+     */
+    public Integer peek() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is currently empty");
+        }
+
+        return data[0];
+    }
+
+    /**
+     * Removes the root of the tree and performs necessary operations to maintain the tree's min heap property
+     *
+     * @return value of the root node (i.e. minimum value in the tree)
+     */
+    public Integer poll() {
+        Integer top = data[0];
+        swap(0, size - 1);
+        data[size - 1] = null;
+        size--;
+        heapifyDown();
+        return top;
+    }
+
+    /**
+     * Builds a list in order top to bottom, left to right of all nodes in the tree
+     *
+     * @return list of ordered nodes currently stored in the min heap tree
+     */
+    public List<Integer> getOrderedNodes() {
+        List<Integer> nodes = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            nodes.add(data[i]);
+        }
+        return nodes;
+    }
+
+    /**
+     * Returns the size of the current min heap tree.
+     *
+     * @return size of the current min heap tree
+     */
+    public int getSize() {
+        return size;
+    }
+
+    private void heapifyDown() {
+        int currentIdx = 0;
+        while (hasLeftChild(currentIdx)) {
+            Integer smallestChildValue = getLeftChild(currentIdx);
+            int smallestChildIdx = getLeftChildIndex(currentIdx);
+
+            if (hasRightChild(currentIdx)) {
+                Integer right = getRightChild(currentIdx);
+                if (right < smallestChildValue) {
+                    smallestChildValue = right;
+                    smallestChildIdx = getRightChildIndex(currentIdx);
+                }
+            }
+
+            Integer currentValue = data[currentIdx];
+            if (currentValue > smallestChildValue) {
+                swap(currentIdx, smallestChildIdx);
+                currentIdx = smallestChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int indexA, int indexB) {
+        Integer temp = data[indexA];
+        data[indexA] = data[indexB];
+        data[indexB] = temp;
+    }
+
+    private Integer getLeftChild(int currentIndex) {
+        return data[getLeftChildIndex(currentIndex)];
+    }
+
+    private int getLeftChildIndex(int currentIndex) {
+        return currentIndex * 2 + 1;
+    }
+
+    private boolean hasLeftChild(int currentIndex) {
+        return isValidIndex(getLeftChildIndex(currentIndex)) && getLeftChild(currentIndex) != null;
+    }
+
+    private boolean isValidIndex(int index) {
+        return index < data.length && index >= 0;
+    }
+
+    private Integer getRightChild(int currentIndex) {
+        return data[getRightChildIndex(currentIndex)];
+    }
+
+    private int getRightChildIndex(int currentIndex) {
+        return currentIndex * 2 + 2;
+    }
+
+    private boolean hasRightChild(int currentIndex) {
+        return isValidIndex(getRightChildIndex(currentIndex)) && getRightChild(currentIndex) != null;
+    }
+
+    private Integer getParent(int currentIndex) {
+        return data[getParentIndex(currentIndex)];
+    }
+
+    private int getParentIndex(int currentIndex) {
+        if (currentIndex == 0) {
+            return -1;
+        }
+        return (currentIndex - 1) / 2;
+    }
+
+    private boolean hasParent(int currentIndex) {
+        return isValidIndex(getParentIndex(currentIndex)) && getParent(currentIndex) != null;
+    }
+}
---
+++ lib/src/test/groovy/com/oscar/datastructure/MinHeapTest.groovy
@@ -0,0 +1,131 @@
+package com.oscar.datastructure
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class MinHeapTest extends Specification {
+
+    @Unroll
+    def "min heap adds nodes successfully and builds a correct min heap tree"() {
+        given:
+        MinHeap minHeap = new MinHeap()
+
+        when: "a series of insertions are performed"
+        for (int n : toInsert) {
+            minHeap.add(n)
+        }
+        def result = minHeap.getOrderedNodes()
+        def resultSize = minHeap.getSize()
+
+        then: "The resulting min heap tree has the correct structure"
+        expected == result
+
+        and: "The min heap tree is of the expected size"
+        expectedSize == resultSize
+
+        where:
+        expected                | toInsert                | expectedSize
+        [1]                     | [1]                     | 1
+        [1, 2, 3, 4, 5, 6]      | [1, 2, 3, 4, 5, 6]      | 6
+        [0, 4, 1, 10, 5, 11, 3] | [10, 4, 11, 3, 5, 1, 0] | 7
+        [-2, 1, 5, 9, 4, 7, 6]  | [9, 4, 7, 1, -2, 6, 5]  | 7
+        [-100, 2, 1, 4, 5, 3]   | [1, 2, 3, 4, 5, -100]   | 6
+        [1, 1, 1, 1, 1]         | [1, 1, 1, 1, 1]         | 5
+    }
+
+    @Unroll
+    def "min heap poll() works as expected and the tree preserves its min heap property"() {
+        given: "an  existing min heap with elements"
+        MinHeap minHeap = new MinHeap()
+        for (int n : toInsert) {
+            minHeap.add(n)
+        }
+
+        when: "a series of deletes are executed"
+        def pollResults = []
+        for (int i = 0; i < numberOfDeletes; i++) {
+            pollResults.add(minHeap.poll())
+        }
+
+        then: "the resulting heap is of the expected size"
+        expectedSize == minHeap.getSize()
+
+        and: "all polled items are present and in the expected order"
+        expectedPolled == pollResults
+
+        and: "the remaining nodes are in the expected order"
+        expectedRemaining == minHeap.getOrderedNodes()
+
+        where:
+        expectedPolled         | toInsert               | numberOfDeletes | expectedSize | expectedRemaining
+        [1]                    | [1, 2, 3, 4]           | 1               | 3            | [2, 4, 3]
+        [1, 2]                 | [1, 2, 3, 4]           | 2               | 2            | [3, 4]
+        [1, 2, 3]              | [1, 2, 3, 4]           | 3               | 1            | [4]
+        [1, 2, 3, 4]           | [1, 2, 3, 4]           | 4               | 0            | []
+        [-2, 1, 4, 5, 6, 7, 9] | [9, 4, 7, 1, -2, 6, 5] | 7               | 0            | []
+        [2, 2, 2, 2, 2, 2]     | [2, 2, 2, 2, 2, 2]     | 6               | 0            | []
+        [2, 2, 2]              | [2, 2, 2, 2, 2, 2]     | 3               | 3            | [2, 2, 2]
+
+    }
+
+    def "peek() throws exception for empty tree"() {
+        given: "an empty heap tree"
+        MinHeap minHeap = new MinHeap()
+
+        when: "peek is called"
+        minHeap.peek()
+
+        then: "an exception is thrown"
+        IllegalStateException exception = thrown()
+        exception.message == "Heap is currently empty"
+    }
+
+    def "peek() is idempotent"() {
+        given: "a min heap tree with some elements"
+        MinHeap minHeap = new MinHeap()
+        for (int num : [9, 4, 7, 1, -2, 6, 5]) {
+            minHeap.add(num)
+        }
+
+        when: "several peek invocations take place"
+        def result
+        for (int i = 0; i < 10; i++) {
+            result = minHeap.peek()
+        }
+
+        then: "the result is the expected value"
+        result == -2
+
+        and: "the min heap maintains its structure"
+        minHeap.getOrderedNodes() == [-2, 1, 5, 9, 4, 7, 6]
+
+        and: "the size remains unchanged"
+        minHeap.getSize() == 7
+    }
+
+    def "min heap capacity grows as needed"() {
+        given: "a heap tree with an initial capacity"
+        MinHeap minHeap = new MinHeap(initialCapacity)
+
+        when: "several inserts take place (beyond initial capacity)"
+        for (int num : [9, 4, 7, 1, -2, 6, 5]) {
+            minHeap.add(num)
+        }
+
+        then: "the heap tree automatically augments its capacity"
+        minHeap.getSize() == 7
+
+        and: "the structure is as expected"
+        minHeap.getOrderedNodes() == [-2, 1, 5, 9, 4, 7, 6]
+
+        where:
+        initialCapacity | _
+        1               | _
+        2               | _
+        3               | _
+        4               | _
+        5               | _
+        6               | _
+        7               | _
+    }
+}
